90a2c7d65359b98ffca6bd10fcbefc0e79bedc03
chaemina <chaemina82@naver.com>
2023-12-28 17:23:22 +0900

    chore : prettier 설치
    tailwind css 기본 설정 및 prettier 설치

--- caramella/tailwind.config.js
+++ caramella/tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        green: {
+          100: "#F2F7F5",
+          200: "#E6F0EB",
+          300: "#CCE1D7",
+          500: "#475D5B",
+          700: "#004643",
+          900: "#00332C",
+        },
+        orange: "#F9BC60",
+        white: "#FFFFFF",
+        black: "#000000",
+        paragraph: "#475d5b",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
