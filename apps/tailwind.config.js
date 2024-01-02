93c5b25287a24230f9cafede974ada285c0d9797
chaemina <chaemina82@naver.com>
2024-01-02 16:50:39 +0900

    feat : tailwind 설정
    color를 지정함

--- apps/tailwind.config.js
+++ apps/tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sky: {
+          50: "#f0f9ff",
+        },
+        blue: {
+          100: "#dbeafe",
+          200: "#93c5fd",
+          300: "#60a5fa",
+          400: "#2563eb",
+        },
+      },
+    },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
