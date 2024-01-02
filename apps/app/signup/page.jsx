409b96ecdac4d15d235489535c0011b36f6abfc0
chaemina <chaemina82@naver.com>
2024-01-02 16:57:23 +0900

    feat : 회원가입 페이지 생성

---
+++ apps/app/signup/page.jsx
@@ -0,0 +1,15 @@
+import SignupTemplate from "../../components/templates/SignupTemplate";
+
+export const metadata = {
+  title: "Caramella | 카라멜라",
+};
+
+const SignupPage = () => {
+  return (
+    <>
+      <SignupTemplate />
+    </>
+  );
+};
+
+export default SignupPage;
