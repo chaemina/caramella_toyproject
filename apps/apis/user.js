42439f8b83968871db1b6798117c96c2dab3f0ee
chaemina <chaemina82@naver.com>
2024-01-04 09:37:32 +0900

    feat : account api 구현
    로그인, 회원가입, 이메일 인증, 인증 연장, 코드 확인 api 연결함

--- apps/apis/user.js
+++ apps/apis/user.js
@@ -1,29 +1,45 @@
 import { instance } from "./instance";
 
-// export const login = (data) => {
-//   const { email, password } = data;
-//   return instance.post("/users/login", {
-//     email,
-//     password,
-//   });
-// };
-
-// mock api
-// export const login = (data) => {
-//   axios
-//     .post("/login")
-//     .then((res) => {
-//       const { data, status } = res;
-//       console.log(data);
-//       console.log(status);
-//     })
-//     .catch((error) => console.log(error));
-// };
-
 export const login = (data) => {
   const { email, password } = data;
   return instance.post("/auth/login", {
     email,
     password,
+  });
+};
+
+export const signup = (data) => {
+  const { email, phone, name, birth, password } = data;
+  return instance.post("/auth/signup", {
+    email,
+    password,
+    phone,
+    name,
+    birth,
+  });
+};
+
+export const emailcheck = (data) => {
+  const { type, email } = data;
+  return instance.post("/auth/email", {
+    type,
+    email,
+  });
+};
+
+export const emailcode = (data) => {
+  const { type, email, code } = data;
+  return instance.post("/auth/verify", {
+    type,
+    email,
+    code,
+  });
+};
+
+export const authextend = (data) => {
+  const { type, email } = data;
+  return instance.post("/auth/extend", {
+    type,
+    email,
   });
 };
